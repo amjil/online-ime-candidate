eaa5f990616a97bd3a433c99d44dc58bc84c1e87
amjil <amjilt@aliyun.com>
2020-02-24 19:06:46 +0800

    add service

---
+++ src/clj/online_ime_candidate/service/suffix.clj
@@ -0,0 +1,15 @@
+(ns online-ime-candidate.service.suffix
+  (:require
+    [clojure.string :as str]
+    [clojure.java.jdbc :as jdbc]))
+
+(def db {:classname   "org.sqlite.JDBC"
+         :subprotocol "sqlite"
+         :subname     "./database.db"})
+
+(defn suffix [table suffix]
+  (let [data (jdbc/query db [(str "select id, char_word from " table " where bqr_biclg like '%" suffix "'")])]
+    (->> data
+         (map :char_word)
+         (str/join "\n")
+         (spit (str table "-" suffix ".txt")))))
