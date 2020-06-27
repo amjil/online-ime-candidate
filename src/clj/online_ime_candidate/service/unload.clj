92d48cbd1a63da90fa31b95af1928596cee425d8
amjil <amjilt@aliyun.com>
2020-06-27 23:26:47 +0800

    add db -> datascript sources

--- src/clj/online_ime_candidate/service/unload.clj
+++ src/clj/online_ime_candidate/service/unload.clj
@@ -34,7 +34,41 @@
          (str/join "\n")
          (spit (str table-name ".txt")))))
 
-(defn select-all [table-name]
+(defn select-table [table-name]
   (let [sql-str (str "select id, char_word as word, bqr_biclg as total, giglgc as simple, active_order, '" table-name "' as tb  from " table-name " where 1=1 and category = 'normal'")
         data (jdbc/query db [sql-str])]
     data))
+
+(def alpha
+  ["a"        "e"        "i"        "m"        "t"        "x"
+   "b"        "f"        "j"        "n"        "q"        "u"        "y"
+   "c"        "g"       "k"        "o"        "r"        "v"        "z"
+   "d"        "h"        "l"        "p"        "s"        "w"])
+
+(defn query-next []
+  (let [sql-str "select t1 || id1 as id1, t2 || id2 as id2 from phrase1 where 1=1"
+        data (->> (jdbc/query db [sql-str])
+                  (group-by :id1)
+                  (map (fn [[k v]] (hash-map k (map :id2 v))))
+                  (into {}))]
+    data))
+
+(defn get-candidate [db indexed id]
+  (let [cand (get db id)]
+    (if cand
+      (map #(get indexed %) cand)
+      [])))
+
+(defn select-all []
+  (let [data (for [x alpha] (select-table x))
+        candidate (query-next)
+        data
+        (->> data
+             flatten
+             (keep-indexed #(assoc %2 :seq %1)))
+        index-data (->> data
+                        (map #(hash-map (str (:tb %) (:id %)) (:seq %)))
+                        (into {}))]
+    (map #(assoc % :child (get-candidate candidate index-data (str (:tb %) (:id %)))) data)))
+
+    ; (group-by #(str (:tb %) (:id %)))))
